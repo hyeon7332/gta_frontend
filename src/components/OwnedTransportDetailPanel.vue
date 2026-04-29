70153e7e4a17514d5ee735fe367cb24806a0d950
hyeon7332 <hyeon7332@gmail.com>
2026-04-29 12:06:25 +0900

    update

--- src/components/OwnedTransportDetailPanel.vue
+++ src/components/OwnedTransportDetailPanel.vue
@@ -37,7 +37,7 @@
       </div>
 
       <div class="mb-4">
-        <div class="h-[260px] rounded-md border border-neutral-700 bg-neutral-800/40 overflow-hidden">
+        <div class="h-[260px] rounded-md border border-neutral-600/70 bg-neutral-800/40 overflow-hidden">
           <img
             v-if="row?.imageUrl"
             :src="resolveImageUrl(row.imageUrl)"
