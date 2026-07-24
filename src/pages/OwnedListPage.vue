0dedbf16c893c34cb5be2fbb759dbcd57bab37c2
hyeon7332 <hyeon7332@gmail.com>
2026-07-24 16:27:30 +0900

    update

--- src/pages/OwnedListPage.vue
+++ src/pages/OwnedListPage.vue
@@ -187,17 +187,32 @@
                         </span>
 
                         <span
-                          class="shrink-0 rounded-md border border-neutral-700
-                                bg-neutral-900/80 px-1.5 py-[3px]
-                                text-[10px] font-semibold leading-none shadow-sm"
+                          :class="[
+                            'shrink-0 rounded-md border px-1.5 py-[3px]',
+                            'text-[10px] font-semibold leading-none shadow-sm',
+                            garage.displaySlotCount > 0 &&
+                            garage.usedSlotCount >= garage.displaySlotCount
+                              ? 'border-blue-500/70 bg-blue-600 text-white'
+                              : 'border-neutral-700 bg-neutral-900/80 text-neutral-300'
+                          ]"
                         >
-                          <span class="text-neutral-300">
+                          <span>
                             {{ garage.usedSlotCount }}
                           </span>
 
-                          <span class="mx-0.5 text-neutral-500">/</span>
+                          <span
+                            :class="[
+                              'mx-0.5',
+                              garage.displaySlotCount > 0 &&
+                              garage.usedSlotCount >= garage.displaySlotCount
+                                ? 'text-blue-200'
+                                : 'text-neutral-500'
+                            ]"
+                          >
+                            /
+                          </span>
 
-                          <span class="text-neutral-300">
+                          <span>
                             {{ garage.displaySlotCount  }}
                           </span>
                         </span>
